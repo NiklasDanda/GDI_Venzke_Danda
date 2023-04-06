600d93cf79c4878cc3565fbcef48cb2943ea49d0
Korvin Venzke <korvin.venzke@gmail.com>
2023-04-06 17:20:12 +0200

    neue layer plus ein layerswitcher panel

--- code/map.js
+++ code/map.js
@@ -1,24 +1,3 @@
-
-// Layer mit Datenquelle
-var osmLayer= new ol.layer.Tile({
-    source: new ol.source.OSM()
-});
-var stamenTerrainLayer = new ol.layer.Tile({
-    source: new ol.source.Stamen({layer: "terrain" })
-});
-var stamenWatercolorLayer = new ol.layer.Tile({
-    source: new ol.source.Stamen({layer: "watercolor" })
-});
-// Stadtteile Hamburg
-var stadtteile = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: "http://geo.local.hcuhh.de/data/districts.geojson",
-        format: new ol.format.GeoJSON()
-    }),
-    maxResolution: 1000,
-    minResolution: 5,
-    opacity: 1
-});
 // Koordinaten der Kartenmitte und
 // Konvertierung WGS84 in Pseudo-Mercator (Bezugssystem von OSM)
 var centerPoint = ol.proj.transform(
@@ -28,7 +7,7 @@ var centerPoint = ol.proj.transform(
 );
 // Kontrollelemente fuer die Karte
 var controls = [
-    new ol.control.Attribution({collapsed: true}),
+    new ol.control.Attribution({ collapsed: true }),
     new ol.control.Zoom(),
     new ol.control.ScaleLine(),
     new ol.control.FullScreen(),
@@ -46,26 +25,107 @@ var view = new ol.View({
     maxZoom: 18,
     minZoom: 4,
 });
-// Kartenerstellung + Verknuepfung mit HTML (id=’map’)
 var map = new ol.Map({
+    target: 'map',
     view: view,
-    layers: [osmLayer, stadtteile],
     controls: controls,
-    target:"map"
+    layers: [
+        new ol.layer.Group({
+            // A layer must have a title to appear in the layerswitcher
+            title: 'Base maps',
+            layers: [
+                new ol.layer.Tile({
+                    title: 'Terrain Labels',
+                    type: 'base',
+                    visible: false,
+                    source: new ol.source.Stamen({
+                        layer: 'terrain-labels'
+                    })
+                }),
+                new ol.layer.Tile({
+                    title: 'Water color',
+                    type: 'base',
+                    visible: false,
+                    source: new ol.source.Stamen({
+                        layer: 'watercolor'
+                    })
+                }),
+                new ol.layer.Tile({
+                    title: 'OSM',
+                    type: 'base',
+                    visible: true,
+                    source: new ol.source.OSM()
+                }),
+                new ol.layer.Tile({
+                    title: 'Dark Matter',
+                    type: 'base',
+                    visible: false,
+                    source: new ol.source.XYZ({
+                        url: 'https://cartodb-basemaps-{a-d}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+                        attributions: '© CARTO'
+                    })
+                }),
+                new ol.layer.Tile({
+                    title: 'Toner',
+                    type: 'base',
+                    visible: false,
+                    source: new ol.source.Stamen({
+                        layer: 'toner'
+                    })
+                }),
+                new ol.layer.Tile({
+                    title: 'World Imagery',
+                    type: 'base',
+                    visible: false,
+                    source: new ol.source.XYZ({
+                        url: 'https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+                        attributions: '© Esri'
+                    })
+                })               
+            ]
+        }),
+        new ol.layer.Group({
+            // A layer must have a title to appear in the layerswitcher
+            title: 'Overlays',
+            // Adding a 'fold' property set to either 'open' or 'close' makes the group layer
+            // collapsible
+            fold: 'open',
+            layers: [
+                // Stadtteile Hamburg
+                new ol.layer.Vector({
+                    title: 'Stadtteile Hamburg',
+                    visible: false,
+                    source: new ol.source.Vector({
+                        url: "http://geo.local.hcuhh.de/data/districts.geojson",
+                        format: new ol.format.GeoJSON()
+                    }),
+                    maxResolution: 1000,
+                    minResolution: 5,
+                    opacity: 1
+                })
+            ]
+        })
+    ]
 });
-// Overlay der Postleitzahlen
-var overlay = new ol.Overlay({
-    element: document.getElementById("overlay"),
-    positioning: "bottom-center"
-});
-map.on("click", function(event) {
-    var coor = event.coordinate;
-    var text = map.forEachFeatureAtPixel(event.pixel, function(feature) {
-        var feature = feature.get("stadtteil_");
-        return feature;
+    // Create the layer switcher control
+    var layerSwitcher = new ol.control.LayerSwitcher({
+        tipLabel: 'Légende', // Optional label for button
+        groupSelectStyle: 'children' // Can be 'children' [default], 'group' or 'none'
     });
-    var element = overlay.getElement();
+    map.addControl(layerSwitcher);
+    // Overlay der Postleitzahlen
+    var overlay = new ol.Overlay({
+        element: document.getElementById("overlay"),
+        positioning: "bottom-center"
+    });
+    map.on("click", function (event) {
+        var coor = event.coordinate;
+        var text = map.forEachFeatureAtPixel(event.pixel, function (feature) {
+            var feature = feature.get("stadtteil_");
+            return feature;
+        });
+        var element = overlay.getElement();
         element.innerHTML = text;
         overlay.setPosition(coor);
         map.addOverlay(overlay);
-});
+    });
