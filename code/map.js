376ad16270615da217dfbbf56f8e8e2bb0c8cde0
Korvin Venzke <korvin.venzke@gmail.com>
2023-04-06 16:40:58 +0200

    script tag nun in eigener js-datei
    - grundgerüst.html gelöscht, da obsolet

--- code/map.js
+++ code/map.js
@@ -1,5 +1,6 @@
+
 // Layer mit Datenquelle
-var osmLayer = new ol.layer.Tile({
+var osmLayer= new ol.layer.Tile({
     source: new ol.source.OSM()
 });
 var stamenTerrainLayer = new ol.layer.Tile({
@@ -8,31 +9,63 @@ var stamenTerrainLayer = new ol.layer.Tile({
 var stamenWatercolorLayer = new ol.layer.Tile({
     source: new ol.source.Stamen({layer: "watercolor" })
 });
-// layercontrol panel
-// var layerSwitcher = new ol.control.LayerSwitcher({
-// 	tipLabel: "Layer" // Optional label for button
-// });
 // Stadtteile Hamburg
 var stadtteile = new ol.layer.Vector({
     source: new ol.source.Vector({
-    url: "http://geo.local.hcuhh.de/data/districts.geojson",
-    format: new ol.format.GeoJSON()
+        url: "http://geo.local.hcuhh.de/data/districts.geojson",
+        format: new ol.format.GeoJSON()
     }),
     maxResolution: 1000,
     minResolution: 5,
     opacity: 1
 });
-var centerPoint = ol.proj.transform([9.993682, 53.551086], "EPSG:4326", "EPSG:3857");  
+// Koordinaten der Kartenmitte und
+// Konvertierung WGS84 in Pseudo-Mercator (Bezugssystem von OSM)
+var centerPoint = ol.proj.transform(
+    [9.990709, 53.531818],
+    "EPSG:4326",
+    "EPSG:3857"
+);
+// Kontrollelemente fuer die Karte
+var controls = [
+    new ol.control.Attribution({collapsed: true}),
+    new ol.control.Zoom(),
+    new ol.control.ScaleLine(),
+    new ol.control.FullScreen(),
+    new ol.control.ZoomSlider(),
+    new ol.control.MousePosition({
+        coordinateFormat: ol.coordinate.createStringXY(4),
+        projection: "EPSG:4326"
+    }),
+];
+// Festlegung der Kartenansicht
 var view = new ol.View({
-        center: centerPoint,
-        zoom: 12,
-        projection: "EPSG:3857",
-        maxZoom: 16,
-        minZoom: 4,
+    center: centerPoint,
+    zoom: 10,
+    projection: "EPSG:3857",
+    maxZoom: 18,
+    minZoom: 4,
 });
+// Kartenerstellung + Verknuepfung mit HTML (id=’map’)
 var map = new ol.Map({
-        view: view,
-        layers: [osmLayer, stadtteile],
-        target: "map",
-        // control: layerSwitcher
+    view: view,
+    layers: [osmLayer, stadtteile],
+    controls: controls,
+    target:"map"
+});
+// Overlay der Postleitzahlen
+var overlay = new ol.Overlay({
+    element: document.getElementById("overlay"),
+    positioning: "bottom-center"
+});
+map.on("click", function(event) {
+    var coor = event.coordinate;
+    var text = map.forEachFeatureAtPixel(event.pixel, function(feature) {
+        var feature = feature.get("stadtteil_");
+        return feature;
+    });
+    var element = overlay.getElement();
+        element.innerHTML = text;
+        overlay.setPosition(coor);
+        map.addOverlay(overlay);
 });
